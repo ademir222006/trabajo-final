12506428dace2cf5fef3d4b56f501f49a09e72ba
ademir222006 <166874551+ademir222006@users.noreply.github.com>
2024-04-26 15:38:37 -0600

    Add files via upload

---
+++ pryecto2/funciones.js
@@ -0,0 +1,39 @@
+var counter=1;
+setInterval(function(){
+	document.getElementById('radio' + counter).checked=true;
+	counter++;
+	if(counter > 6){
+		counter = 1;
+	}
+},2000);
+
+window.addEventListener("load", function() {
+	document.getElementById('bamban').addEventListener("click",function() {
+		alert("QUE BUSCAS FEO AJJAJAJAJ  MENTIRA SAQUEME 10 PROFE PA NO CARGAR PIEDRA");
+	})
+});
+window.addEventListener("load", function() {
+	document.getElementById('banban').addEventListener("click",function() {
+		alert("QUE BUSCAS FEO AJJAJAJAJ  MENTIRA SAQUEME 10 PROFE PA NO CARGAR PIEDRA");
+	})
+});
+window.addEventListener("load", function() {
+	document.getElementById('banbann').addEventListener("click",function() {
+		alert("QUE BUSCAS FEO AJJAJAJAJ  MENTIRA SAQUEME 10 PROFE PA NO CARGAR PIEDRA");
+	})
+});
+window.addEventListener("load", function() {
+	document.getElementById('banbannn').addEventListener("click",function() {
+		alert("QUE BUSCAS FEO AJJAJAJAJ  MENTIRA SAQUEME 10 PROFE PA NO CARGAR PIEDRA");
+	})
+});
+window.addEventListener("load", function() {
+	document.getElementById('banbannnn').addEventListener("click",function() {
+		alert("QUE BUSCAS FEO AJJAJAJAJ  MENTIRA SAQUEME 10 PROFE PA NO CARGAR PIEDRA");
+	})
+});
+window.addEventListener("load", function() {
+	document.getElementById('banbannnnn').addEventListener("click",function() {
+		alert("QUE BUSCAS FEO AJJAJAJAJ  MENTIRA SAQUEME 10 PROFE PA NO CARGAR PIEDRA");
+	})
+});
